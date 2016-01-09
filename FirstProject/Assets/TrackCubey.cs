01f9d7e521d61650e4301a74fb738f88493addcf
Estevan Hernandez <estevan+github@bluerocket.us>
2016-01-08 23:45:18 -0800

    UNITY
    YES

---
+++ FirstProject/Assets/TrackCubey.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class TrackCubey : MonoBehaviour {
+	public GameObject target;
+	public float xOffset = -4;
+	public float yOffset = 4;
+	public float zOffset = -4;
+	// Use this for initialization
+	void Start () {
+		gameObject.transform.position = new Vector3 (target.transform.position.x + xOffset,
+			target.transform.position.y + yOffset,
+			target.transform.position.z + zOffset);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+	void LateUpdate() {
+		gameObject.transform.position = new Vector3 (target.transform.position.x + xOffset,
+			target.transform.position.y + yOffset,
+			target.transform.position.z + zOffset);
+	}
+}
---
+++ FirstProject/Assets/WASDMove.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WASDMove : MonoBehaviour {
+	public float moveSpeed = 0.2F;
+	public float jumpSpeed = 1.0F;
+	bool recording;
+	long lifeSpan = 0;
+	// Use this for initialization
+	void Start () {
+		recording = true;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (recording && (Input.GetMouseButton(0))==false) {
+			Debug.Log ("recording && (Input.GetMouseButton(0))) == true");
+			lifeSpan++;
+			if(Input.GetKey(KeyCode.A)) {
+				Vector3 temp = new Vector3(-moveSpeed,0,0);
+				gameObject.transform.position += temp;
+			}
+			if(Input.GetKey(KeyCode.D)) {
+				Vector3 temp = new Vector3(moveSpeed,0,0);
+				gameObject.transform.position += temp;
+			}
+			if(Input.GetKeyDown(KeyCode.W)) {
+				Vector3 temp = new Vector3(0,jumpSpeed,0);
+				gameObject.transform.position += temp;
+			}
+		}
+		if (Input.GetMouseButtonDown(0)) {
+			recording = false;
+			Debug.Log ("LMB Clicked");
+		}
+	}
+}
