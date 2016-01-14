1320ab2e8bc694ddba140b6a6505dc7b282af5aa
Estevan Hernandez <estevan+github@bluerocket.us>
2016-01-14 01:16:10 -0800

    YAY SORTA WORKING
    the guy can move, removed time travel code for now.

---
+++ FirstProject/Assets/Scripts/CharacterMover.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterMover : MonoBehaviour {
+	public float speed = 6.0F;
+	public float jumpSpeed = 8.0F;
+	public float gravity = 20.0F;
+	private Vector3 moveDirection = Vector3.zero;
+	void Start() {
+	}
+	void Update() {
+		int leftOrRight = 0;
+		CharacterController controller = GetComponent<CharacterController>();
+		if (controller.isGrounded) {
+			//moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+			if (Input.GetKey ("d")) {
+				Debug.Log ("D Pressed");
+				leftOrRight++;
+			}
+			if (Input.GetKey ("a")) {
+				Debug.Log ("A Pressed");
+				leftOrRight--;
+			}
+			Debug.Log ("leftOrRight is" + leftOrRight);
+			moveDirection = new Vector3 (leftOrRight, 0, Input.GetAxis ("Vertical"));
+			moveDirection = transform.TransformDirection(moveDirection);
+			moveDirection *= speed;
+			if (Input.GetButton ("Jump")) {
+				moveDirection.y = jumpSpeed;
+			}
+			Debug.Log ("Input.GetAxis(\"Vertical\")); is" + Input.GetAxis ("Vertical"));
+			Debug.Log ("Input.GetAxis(\"Horizontal\")); is" + Input.GetAxis ("Horizontal"));
+			Debug.Log ("Input.GetButton(\"Jump\")); is" + Input.GetButton ("Jump"));
+
+		}
+		moveDirection.y -= gravity * Time.deltaTime;
+		controller.Move(moveDirection * Time.deltaTime);
+
+	}
+}
--- FirstProject/Assets/Scripts/WASDMove.cs
+++ FirstProject/Assets/Scripts/WASDMove.cs
@@ -2,61 +2,73 @@
 using System.Collections;
 
 public class WASDMove : MonoBehaviour {
+	//public Rigidbody rg;
 	public float moveSpeed = 0.2F;
 	public float jumpSpeed = 1.0F;
 	public int birthDay;
-	public int deadthDay;
-	bool recording;
-	long lifeSpan = 0;
-	struct history {
-		int frame;
-		bool W;
-		bool A;
-		bool S;
-		bool D;
+	public int deathDay = 0;
+	public struct etheralState {
+		public bool recording;
+		public bool placing;
+		public bool playing;
 	}
-
-	bool isInput() {
-		
-		return true;
-
+	public etheralState myEtheralState = new etheralState();
+	public int lifeSpan = 0;
+	public struct vectorHistory {
+		public Vector2 vector;
 	}
-
-
-
-	// Use this for initialization
+	public vectorHistory[] vectors = new vectorHistory[60 * 60 * 5];
 	void Start () {
-		recording = true;
+		//rg = gameObject.AddComponent<Rigidbody>();
+		if (GameObject.FindGameObjectsWithTag ("Bunny").Length == 1) {
+			Debug.Log ("There is one bunny");
+			//myEtheralState.recording = true;
+		} else {
+			//
+		}
+		myEtheralState.recording = true;
 		birthDay = Time.frameCount;
-		history[] history = new history[60 ^ 3];
-		
 	}
-	
-	// Update is called once per frame
 	void Update () {
-		if (recording && (Input.GetMouseButton(0))==false) {
-			Debug.Log ("recording && (Input.GetMouseButton(0))) == true");
-			lifeSpan++;
-			if(Input.GetKey(KeyCode.A)) {
-				Vector3 temp = new Vector3(-moveSpeed,0,0);
+		if (myEtheralState.recording) {
+			if (Input.GetKey (KeyCode.A)) {
+				Vector3 temp = new Vector3 (-moveSpeed, 0, 0);
 				gameObject.transform.position += temp;
 			}
-			if(Input.GetKey(KeyCode.D)) {
-				Vector3 temp = new Vector3(moveSpeed,0,0);
+			if (Input.GetKey (KeyCode.D)) {
+				Vector3 temp = new Vector3 (moveSpeed, 0, 0);
 				gameObject.transform.position += temp;
 			}
-			if(Input.GetKeyDown(KeyCode.W)) {
-				Vector3 temp = new Vector3(0,jumpSpeed,0);
-				gameObject.transform.position += temp;
+			if (Input.GetKeyDown (KeyCode.W)) {
+				//Vector3 temp = new Vector3 (0, jumpSpeed, 0);
+				//gameObject.transform.position += temp;
+				//rg.AddForce(temp);
+			}
+			vectors [lifeSpan].vector = new Vector2 (gameObject.transform.position.x, gameObject.transform.position.y);
+			if (Input.GetMouseButtonDown (0)) {
+				myEtheralState.recording = false;
+				myEtheralState.placing = true;
+				deathDay = lifeSpan;
+				lifeSpan = 0;
+				Debug.Log ("LMB Clicked Down");
 			}
 		}
-		if (Input.GetMouseButtonDown(0)) {
-			recording = false;
-			Debug.Log ("LMB Clicked Down");
+		if (myEtheralState.playing) {
+			gameObject.transform.position = vectors [lifeSpan].vector;
 		}
 		if (Input.GetMouseButtonUp (0)) {
 			Debug.Log ("LMB Clicked Up");
+			myEtheralState.placing = false;
+			myEtheralState.playing = true;
 		}
-		Debug.Log ("lifeSpan is" + lifeSpan + "x is %" + gameObject.transform.position.x + "y is %" + gameObject.transform.position.y);
+		if (lifeSpan > deathDay && deathDay != 0) {
+			gameObject.SetActive(false);//
+		}
+		if (myEtheralState.placing) {
+			Debug.Log ("You have stopped the sands of time");
+		} else {
+			Debug.Log ("The Wheel keeps on burning");
+			lifeSpan += 1;
+		}
 	}
 }
