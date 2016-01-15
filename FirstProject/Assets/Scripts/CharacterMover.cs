f2b86ce36202a105f6b60355d4c3129d8851f04b
Estevan Hernandez <estevan@bluerocket.us>
2016-01-15 01:20:54 -0800

    Starting on the VCR code.
    need to learn about classes, how can I make the VCR it’s own component
    or object attached to gameObject? that way I can pass messages to play,
    pause or record, instead of doing it in a struct.

--- FirstProject/Assets/Scripts/CharacterMover.cs
+++ FirstProject/Assets/Scripts/CharacterMover.cs
@@ -2,39 +2,64 @@
 using System.Collections;
 
 public class CharacterMover : MonoBehaviour {
-	public float speed = 6.0F;
-	public float jumpSpeed = 8.0F;
-	public float gravity = 20.0F;
+	public struct lifeInfo {
+		public int birthFrame;
+		public int lifeFrames;
+		public int deathFrame;
+		
+	}
+	public lifeInfo myLifeInfo;
+	public float speed;
+	public float jumpSpeed;
+	public float gravity;
+	public struct stateStruct {
+		public bool pause;
+		public bool record;
+		public bool play;
+	}
+	public Vector3[] locationHistory = new Vector3[0];
+	public stateStruct bunnyState;
 	private Vector3 moveDirection = Vector3.zero;
 	void Start() {
+		bunnyState.pause = false;
+		bunnyState.record = true;
+		bunnyState.play = false;
+		myLifeInfo.birthFrame = Time.frameCount;
+		myLifeInfo.lifeFrames = 0;
 	}
 	void Update() {
-//		int leftOrRight = 0;
 		CharacterController controller = GetComponent<CharacterController>();
-		if (controller.isGrounded) {
-			moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-//			if (Input.GetKey ("d")) {
-//				Debug.Log ("D Pressed");
-//				leftOrRight++;
-//			}
-//			if (Input.GetKey ("a")) {
-//				Debug.Log ("A Pressed");
-//				leftOrRight--;
-//			}
-//			Debug.Log ("leftOrRight is" + leftOrRight);
-//			moveDirection = new Vector3 (leftOrRight, 0, Input.GetAxis ("Vertical"));
-			moveDirection = transform.TransformDirection(moveDirection);
-			moveDirection *= speed;
-			if (Input.GetButton ("Jump")) {
-				moveDirection.y = jumpSpeed;
+		//if recording to VCR
+		if (bunnyState.record && !bunnyState.play && !bunnyState.pause) {
+			
+			if (controller.isGrounded) {
+				moveDirection = new Vector3(-Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+				moveDirection = transform.TransformDirection(moveDirection);
+				moveDirection *= speed;
+				if (Input.GetButtonDown ("Jump")) {
+					moveDirection.y = jumpSpeed;
+				}
+				Debug.Log ("Input.GetAxis(\"Vertical\")); is" + Input.GetAxis ("Vertical"));
+				Debug.Log ("Input.GetAxis(\"Horizontal\")); is" + Input.GetAxis ("Horizontal"));
+				Debug.Log ("Input.GetButton(\"Jump\")); is" + Input.GetButtonDown ("Jump"));
+
 			}
-			Debug.Log ("Input.GetAxis(\"Vertical\")); is" + Input.GetAxis ("Vertical"));
-			Debug.Log ("Input.GetAxis(\"Horizontal\")); is" + Input.GetAxis ("Horizontal"));
-			Debug.Log ("Input.GetButton(\"Jump\")); is" + Input.GetButton ("Jump"));
+			moveDirection.y -= gravity * Time.deltaTime;
+			controller.Move(moveDirection * Time.deltaTime);
+			locationHistory = new Vector3[myLifeInfo.lifeFrames];
+			locationHistory [myLifeInfo.lifeFrames] = gameObject.transform.position;
+			myLifeInfo.lifeFrames++;
+		}
+		//if playing VCR
+		if (!bunnyState.record && bunnyState.play && !bunnyState.pause) {
+			
+		}
+		//if placing the player object
+		if (!bunnyState.record && !bunnyState.play && bunnyState.pause) {
 
 		}
-		moveDirection.y -= gravity * Time.deltaTime;
-		controller.Move(moveDirection * Time.deltaTime);
+
+
 
 	}
 }
--- FirstProject/Assets/Scripts/Track.cs
+++ FirstProject/Assets/Scripts/Track.cs
@@ -14,16 +14,7 @@ public class Track : MonoBehaviour {
 	public float yRotate;
 
 	bool rotate;
-	// Use this for initialization
-	void Start () {
-		gameObject.transform.position = new Vector3 (target.transform.position.x + xOffset,
-			target.transform.position.y + yOffset,
-			target.transform.position.z + zOffset);
 
-	}
-		
-	
-	// Update is called once per frame
 	void Update () {
 	}
 
@@ -31,19 +22,23 @@ public class Track : MonoBehaviour {
 		gameObject.transform.position = new Vector3 (target.transform.position.x + xOffset,
 			target.transform.position.y + yOffset,
 			target.transform.position.z + zOffset);
-		if (Input.GetKeyDown ("o")) {
-			rotate = rotate ? false : true;
-			Debug.Log ("rotate is:" + rotate);
-			switch (rotate) {
-			case true:
-				gameObject.transform.RotateAround(target.transform.position, new Vector3(target.transform.position.x+2,target.transform.position.y,target.transform.position.z),35.264F);
-				gameObject.transform.RotateAround(target.transform.position, new Vector3(target.transform.position.x,target.transform.position.y+2,target.transform.position.z),45);
-				break;
-			case false: 
-				gameObject.transform.RotateAround(target.transform.position, new Vector3(target.transform.position.x,target.transform.position.y+2,target.transform.position.z),-45);
-				gameObject.transform.RotateAround(target.transform.position, new Vector3(target.transform.position.x+2,target.transform.position.y,target.transform.position.z),-35.264F);
-				break;
-			}
-		}
+//		if (Input.GetKeyDown ("o")) {
+//			rotate = rotate ? false : true;
+//			Debug.Log ("rotate is:" + rotate);
+//			switch (rotate) {
+//			case true:
+//				//gameObject.transform.RotateAround(target.transform.position, new Vector3(target.transform.position.x+2,target.transform.position.y,target.transform.position.z),35.264F);
+//				//gameObject.transform.RotateAround(target.transform.position, new Vector3(target.transform.position.x,target.transform.position.y+2,target.transform.position.z),45);
+//				//gameObject.transform.RotateAround(new Vector3(target.transform.position.x-1,target.transform.position.y-1,target.transform.position.z-1), new Vector3(target.transform.position.x+1,target.transform.position.y+1,target.transform.position.z+1),45);
+//				gameObject.transform.RotateAround(new Vector3(target.transform.position.x,target.transform.position.y,target.transform.position.z), new Vector3(target.transform.position.x,target.transform.position.y+10,target.transform.position.z),45);
+//				break;
+//			case false: 
+//				//gameObject.transform.RotateAround(target.transform.position, new Vector3(target.transform.position.x,target.transform.position.y+2,target.transform.position.z),-45);
+//				//gameObject.transform.RotateAround(target.transform.position, new Vector3(target.transform.position.x+2,target.transform.position.y,target.transform.position.z),-35.264F);
+//				//gameObject.transform.RotateAround(new Vector3(target.transform.position.x-1,target.transform.position.y-1,target.transform.position.z-1), new Vector3(target.transform.position.x+1,target.transform.position.y+1,target.transform.position.z+1),45);
+//				gameObject.transform.RotateAround(new Vector3(target.transform.position.x,target.transform.position.y,target.transform.position.z), new Vector3(target.transform.position.x,target.transform.position.y+10,target.transform.position.z),-45);
+//				break;
+//			}
+//		}
 	}
 }
